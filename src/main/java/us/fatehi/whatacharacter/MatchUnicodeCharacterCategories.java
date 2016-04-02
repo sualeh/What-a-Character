d10d26dd6f491487ad87bb3f41849a9b246b4728
Sualeh Fatehi <sualeh@hotmail.com>
2016-04-01 21:03:37 -0400

    how to use regular expressions that can match Unicode character categories

--- src/main/java/us/fatehi/whatacharacter/MatchUnicodeCharacterCategories.java
+++ src/main/java/us/fatehi/whatacharacter/MatchUnicodeCharacterCategories.java
@@ -18,16 +18,22 @@ package us.fatehi.whatacharacter;
 
 import java.util.regex.Pattern;
 
-public class MatchUnicodeStrings
+public class MatchUnicodeCharacterCategories
 {
 
   public static void main(String[] args)
   {
+    // Demonstrates how to use regular expressions that can match
+    // Unicode character categories
+
     final Pattern digit = Pattern.compile("[0-9]*");
     final Pattern unicode_digit = Pattern.compile("\\p{Nd}*");
     final String hindiNumber = "१२३४५६७८९०";
-    System.out.println(String.format("digit match = %s", digit.matcher(hindiNumber).matches()));
-    System.out.println(String.format("Unicode digit match = %s", unicode_digit.matcher(hindiNumber).matches()));
+    System.out.println(String.format("digit match = %s",
+                                     digit.matcher(hindiNumber).matches()));
+    System.out
+      .println(String.format("Unicode digit match = %s",
+                             unicode_digit.matcher(hindiNumber).matches()));
   }
 
 }
