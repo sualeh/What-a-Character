47fa9ddb6148a7f65b05c185cf1bf197dfe8029b
Sualeh Fatehi <sualeh@hotmail.com>
2016-06-23 20:51:04 -0400

    More characters

--- src/main/java/us/fatehi/whatacharacter/UnicodeCharLiterals.java
+++ src/main/java/us/fatehi/whatacharacter/UnicodeCharLiterals.java
@@ -24,11 +24,14 @@ public class UnicodeCharLiterals
     // How to use Unicode characters in string and character literals.
     // The hexadecimal values can be in uppercase or lowercase. The
     // characters are always in the Basic Multilingual Plane.
-    
+
     char ch1 = 'a';
     char ch2 = '编';
-    char ch3 = '\u00ea';
-    
+    // char ch3 = '𝒮';
+    // (Not a BMP character!)
+    // char ch4 = '\';
+    // (Backslash is a syntax error!)
+
   }
 
 }
