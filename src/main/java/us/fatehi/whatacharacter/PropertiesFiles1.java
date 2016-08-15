5bddde41516db893f94013011ed3364ddc2bd6c9
Sualeh Fatehi <sualeh@hotmail.com>
2016-08-14 23:16:05 -0400

    Adding ability to read properties file in any encoding

---
+++ src/main/java/us/fatehi/whatacharacter/PropertiesFiles1.java
@@ -0,0 +1,23 @@
+package us.fatehi.whatacharacter;
+
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.Properties;
+
+public class PropertiesFiles1
+{
+
+  public static void main(final String args[])
+    throws Exception
+  {
+    Properties properties = new Properties();
+    InputStream stream = PropertiesFiles1.class
+      .getResourceAsStream("properties1.properties");
+    properties.load(new InputStreamReader(stream, StandardCharsets.UTF_8));
+
+    System.out.println(properties.get("property1"));
+  }
+
+}
