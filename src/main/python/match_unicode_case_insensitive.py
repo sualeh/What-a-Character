320e0985f08ac4932afa74981bd91f7befe916f0
Sualeh Fatehi <sualeh@hotmail.com>
2024-01-25 08:05:46 -0500

    Update Java and Python code for number pattern matching

--- src/main/python/match_unicode_case_insensitive.py
+++ src/main/python/match_unicode_case_insensitive.py
@@ -17,23 +17,30 @@ http://creativecommons.org/licenses/by-sa/4.0/.
 import re
 import regex
 
-# Demonstrates how to use regular expressions that can match
-# Unicode characters in a case-insensitive fashion
 
-lower_greek = "σκύλος"
-upper_greek = "ΣΚΎΛΟΣ"
-pattern_greek = re.compile(lower_greek, re.IGNORECASE)
-matches = bool(pattern_greek.match(upper_greek))
-print(
-    f"Unicode case-insensitive match for Greek words {lower_greek} = {upper_greek} is {matches}"
-)
+def main():
 
-# When a lower-case character results in more than one uppercase character,
-# there is no match
-lower_german = "straße"
-upper_german = "STRASSE"
-pattern_german = regex.compile(lower_german, regex.IGNORECASE | regex.UNICODE)
-matches = bool(pattern_german.match(upper_german))
-print(
-    f"Unicode case-insensitive match for German word {lower_german} = {upper_german} is {matches}"
-)
+    # Demonstrates how to use regular expressions that can match
+    # Unicode characters in a case-insensitive fashion
+
+    lower_greek = "σκύλος"
+    upper_greek = "ΣΚΎΛΟΣ"
+    pattern_greek = re.compile(lower_greek, re.IGNORECASE)
+    matches = bool(pattern_greek.match(upper_greek))
+    print(
+        f"Unicode case-insensitive match for Greek words {lower_greek} = {upper_greek} is {matches}"
+    )
+
+    # When a lower-case character results in more than one uppercase character,
+    # there is no match
+    lower_german = "straße"
+    upper_german = "STRASSE"
+    pattern_german = regex.compile(lower_german, regex.IGNORECASE | regex.UNICODE)
+    matches = bool(pattern_german.match(upper_german))
+    print(
+        f"Unicode case-insensitive match for German word {lower_german} = {upper_german} is {matches}"
+    )
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/main/python/part1/match_unicode_character_categories.py
@@ -0,0 +1,53 @@
+"""
+
+What a Character
+https://github.com/sualeh/What-a-Character
+Copyright (c) 2016-2024, Sualeh Fatehi.
+
+This library is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+This work is licensed under the Creative Commons Attribution-ShareAlike
+4.0 International License. To view a copy of this license, visit
+http://creativecommons.org/licenses/by-sa/4.0/.
+
+"""
+
+import re
+import regex
+
+
+def main():
+    # Demonstrates number parsing functions can
+    # handle numeric values from other (non-English)
+    # languages.
+
+    # ----------------------------------------
+    # Handle Unicode numbers
+
+    hindi_number = "१२३४५६७८९०"
+
+    # Non-Unicode matching
+    digit_regex = r"[0-9]+"
+    match_result = re.match(digit_regex, hindi_number)
+    matches = bool(match_result)
+    print(f"Match found for {digit_regex} - {matches}")
+
+    # Using digit character class
+    digit_regex = r"\d+"
+    match_result = re.match(digit_regex, hindi_number)
+    matches = bool(match_result)
+    print(f"Match found for {digit_regex} - {matches}")
+
+    # Using Unicode character categories
+    digit_regex = r"\p{Nd}+"
+    match_result = regex.match(digit_regex, hindi_number)
+    matches = bool(match_result)
+    print(f"Match found for {digit_regex} - {matches}")
+
+    # ----------------------------------------
+
+
+if __name__ == "__main__":
+    main()
